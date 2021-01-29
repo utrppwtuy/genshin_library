719aeb12d81afe05a8150987373d1fe432bb90d9
zhangliang <zhangliang2019@bhh.com.cn>
2021-01-29 16:10:31 +0800

    后端工程新建

---
+++ 80_code/JAVA/genshin-library-service/src/main/java/org/xiyue/genshinlibraryservice/GenshinLibraryServiceApplication.java
@@ -0,0 +1,13 @@
+package org.xiyue.genshinlibraryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenshinLibraryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenshinLibraryServiceApplication.class, args);
+	}
+
+}
