bfbdf835f5134a7755dbeb9795078ae1f7e9b2d1
zhangliang <zhangliang2019@bhh.com.cn>
2021-03-29 18:25:26 +0800

    新增主页面

---
+++ 80_code/VUE/genshin_library/src/page/home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <el-container>
+      <el-header>
+        {{ headTitle }}</el-header>
+      <el-main> 1</el-main>
+      <el-footer>
+        {{ footerTitle }}
+      </el-footer>
+    </el-container>
+  </div>
+</template>
+<script>
+// import anime from 'animejs/lib/anime.es.js'
+export default {
+  data () {
+    return {
+      headTitle: '希月genshin试验场',
+      footerTitle:
+        "I am not a designer nor a coder. I'm just a guy with a point-of-view and a computer."
+    }
+  },
+  mounted () {}
+}
+</script>
+
+<style>
+.el-header {
+  background-color: #f38181;
+  height: 240px !important;
+  font-size: 48px;
+  color: white;
+  text-align: center;
+}
+.el-footer {
+  background-color: #95e1d3;
+  height: 120px !important;
+  font-size: 24px;
+  color: white;
+  text-align: center;
+}
+
+</style>
--- 80_code/VUE/genshin_library/src/router/index.js
+++ 80_code/VUE/genshin_library/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -8,8 +7,45 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: resolve => {
+        require(['@/page/home'], resolve)
+      }
+    },
+    {
+      path: '/role',
+      name: 'role',
+      component: resolve => {
+        require(['@/page/role/role'], resolve)
+      }
+    },
+    {
+      path: '/space',
+      name: 'space',
+      component: resolve => {
+        require(['@/page/space/space'], resolve)
+      }
+    },
+    {
+      path: '/time',
+      name: 'time',
+      component: resolve => {
+        require(['@/page/time/time'], resolve)
+      }
+    },
+    {
+      path: '/game',
+      name: 'game',
+      component: resolve => {
+        require(['@/page/game/game'], resolve)
+      }
+    },
+    {
+      path: '/anime',
+      name: 'anime',
+      component: resolve => {
+        require(['@/page/anime/anime'], resolve)
+      }
     }
   ]
 })
