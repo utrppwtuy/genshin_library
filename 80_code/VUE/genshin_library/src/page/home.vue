e5209fe7e557b23e955d03ec475b13f3eb66ddf2
zhangliang <zhangliang2019@bhh.com.cn>
2021-03-30 18:25:50 +0800

    主页修改

--- 80_code/VUE/genshin_library/src/page/home.vue
+++ 80_code/VUE/genshin_library/src/page/home.vue
@@ -2,10 +2,15 @@
   <div>
     <el-container>
       <el-header>
-        {{ headTitle }}</el-header>
-      <el-main> 1</el-main>
+        <div class="headTitle">{{ headTitle }}</div>
+      </el-header>
+      <el-main>
+        <div class="mainTitle">
+          <el-link class="linkType" href="#/anime">{{ mainTitle[0] }}</el-link>
+        </div>
+      </el-main>
       <el-footer>
-        {{ footerTitle }}
+        <div class="footerTitle">{{ footerTitle }}</div>
       </el-footer>
     </el-container>
   </div>
@@ -16,6 +21,7 @@ export default {
   data () {
     return {
       headTitle: '希月genshin试验场',
+      mainTitle: ['XY001 js全自动手书动画'],
       footerTitle:
         "I am not a designer nor a coder. I'm just a guy with a point-of-view and a computer."
     }
@@ -24,20 +30,45 @@ export default {
 }
 </script>
 
-<style>
+<style scope>
 .el-header {
   background-color: #f38181;
-  height: 240px !important;
-  font-size: 48px;
-  color: white;
-  text-align: center;
-}
-.el-footer {
-  background-color: #95e1d3;
-  height: 120px !important;
-  font-size: 24px;
-  color: white;
-  text-align: center;
+  height: 0% !important;
 }
 
+.el-footer {
+  background-color: white;
+  height: 0% !important;
+  color: white;
+}
+.headTitle {
+  color: white;
+  font-size: 64px;
+  padding-top: 10%;
+  padding-bottom: 30px;
+  text-align: center;
+}
+.mainTitle {
+  padding-top: 5%;
+  text-align: center;
+}
+.linkType {
+   font-size: 32px;
+}
+.el-link.el-link--default {
+    color: #f38181;
+}
+.el-link.el-link--default:hover {
+    color: #fce38a;
+}
+.el-link.el-link--default:after, .el-link.el-link--primary.is-underline:hover:after, .el-link.el-link--primary:after {
+    border-color: #fce38a !important;
+}
+.footerTitle {
+  color: #95e1d3;
+  font-size: 24px;
+  text-align: center;
+  padding-top: 20%;
+  padding-bottom: 40px;
+}
 </style>
